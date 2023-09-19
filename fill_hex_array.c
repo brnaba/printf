a3ccbc46a4d1b76aab8650cc007d57ec17175dff
Barnaba <39010017+brnaba@users.noreply.github.com>
2023-09-19 23:48:33 +0200

    Update fill_hex_array.c

--- fill_hex_array.c
+++ fill_hex_array.c
@@ -3,12 +3,12 @@
 /**
  * fill_hex_array - writes the character c to stdout
  *
- * @bnr: array where is stored the binary.
- * @hex: array where is stored the hexadecimal.
+ * @bnr: array where is stored the binary
+ * @hex: array where is stored the hexadecimal
  * @isupp: integer that determines if the hexadecimal array is
  * in uppercase or lowercase letter.
  * @limit: size of hex
- * Return: binary array.
+ * Return: binary array
  */
 char *fill_hex_array(char *bnr, char *hex, int isupp, int limit)
 {
