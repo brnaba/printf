395256608f958739f6059033f707a1dfcafb2c86
Barnaba <39010017+brnaba@users.noreply.github.com>
2023-09-20 00:15:13 +0200

    Update print_usr.c

--- print_usr.c
+++ print_usr.c
@@ -2,10 +2,10 @@
 
 /**
  * print_usr - prints a string and values of
- * non-printed chars
- * @arguments: input string
- * @buf: buffer pointer
- * @ibuf: index for buffer pointer
+ * non-printed chars.
+ * @arguments: input string.
+ * @buf: buffer pointer.
+ * @ibuf: index for buffer pointer.
  * Return: number of chars printed
  */
 int print_usr(va_list arguments, char *buf, unsigned int ibuf)
