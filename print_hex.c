78d05f2537fff5707430aa72b10a69d3af54f322
Barnaba <39010017+brnaba@users.noreply.github.com>
2023-09-20 00:02:44 +0200

    Update print_hex.c

--- print_hex.c
+++ print_hex.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * print_hex - prints a decimal in hexadecimal
- * @arguments: input string
- * @buf: buffer pointer
- * @ibuf: index for buffer pointer
- * Return: number of chars printed
+ * print_hex - prints a decimal in hexadecimal.
+ * @arguments: input string.
+ * @buf: buffer pointer.
+ * @ibuf: index for buffer pointer.
+ * Return: number of chars printed.
  */
 int print_hex(va_list arguments, char *buf, unsigned int ibuf)
 {
