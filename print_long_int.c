7021201ec84d22758d6b7035fcdd0ed282ea785f
Barnaba <39010017+brnaba@users.noreply.github.com>
2023-09-20 00:06:17 +0200

    Update print_long_int.c

--- print_long_int.c
+++ print_long_int.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * prinlint - prints a long integer
- * @arguments: input string
- * @buf: buffer pointer
- * @ibuf: index for buffer pointer
+ * prinlint - prints a long integer.
+ * @arguments: input string.
+ * @buf: buffer pointer.
+ * @ibuf: index for buffer pointer.
  * Return: number of chars printed.
  */
 int prinlint(va_list arguments, char *buf, unsigned int ibuf)
