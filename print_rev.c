7eec8771fcec9a8760c98e6dac9195bb35fc2119
Barnaba <39010017+brnaba@users.noreply.github.com>
2023-09-20 00:11:43 +0200

    Update print_rev.c

--- print_rev.c
+++ print_rev.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_rev - writes the str in reverse
- * @arguments: input string
- * @buf: buffer pointer
- * @ibuf: index for buffer pointer
+ * print_rev - writes the str in reverse.
+ * @arguments: input string.
+ * @buf: buffer pointer.
+ * @ibuf: index for buffer pointer.
  * Return: number of chars printed.
  */
 int print_rev(va_list arguments, char *buf, unsigned int ibuf)
