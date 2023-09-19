fb870e6ae46d7a00fff5c39179e4a157ef6b2a21
Barnaba <39010017+brnaba@users.noreply.github.com>
2023-09-20 00:17:09 +0200

    Update print_str.c

--- print_str.c
+++ print_str.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_str - writes the string to stdout
- * @arguments: input string
- * @buf: buffer pointer
- * @ibuf: index for buffer pointer
+ * print_str - writes the string to stdout.
+ * @arguments: input string.
+ * @buf: buffer pointer.
+ * @ibuf: index for buffer pointer.
  * Return: On success 1.
  */
 int print_str(va_list arguments, char *buf, unsigned int ibuf)
