115e884d636c02d5057ea1f8a647e0366be69d12
Barnaba <39010017+brnaba@users.noreply.github.com>
2023-09-19 23:47:17 +0200

    Update fill_binary_array.c

--- fill_binary_array.c
+++ fill_binary_array.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * fill_binary_array - prints decimal in binary
- * @binary: pointer to binary
- * @int_in: input number
- * @isneg: if input number is negative
- * @limit: size of the binary
+ * fill_binary_array - prints decimal in binary.
+ * @binary: pointer to binary.
+ * @int_in: input number.
+ * @isneg: if input number is negative.
+ * @limit: size of the binary.
  * Return: number of chars printed.
  */
 char *fill_binary_array(char *binary, long int int_in, int isneg, int limit)
