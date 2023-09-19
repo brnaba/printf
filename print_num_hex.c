204d5a058b6a79af1d4f91729e461b6ea12937ca
Barnaba <39010017+brnaba@users.noreply.github.com>
2023-09-20 00:14:31 +0200

    Update print_num_hex.c

--- print_num_hex.c
+++ print_num_hex.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * prinnhex - print number in hex begining with zero
- * @arguments: input string
- * @buf: buffer pointer
- * @ibuf: index for buffer pointer
- * Return: number of chars printed
+ * prinnhex - print number in hex begining with zero.
+ * @arguments: input string.
+ * @buf: buffer pointer.
+ * @ibuf: index for buffer pointer.
+ * Return: number of chars printed.
  */
 int prinnhex(va_list arguments, char *buf, unsigned int ibuf)
 {
