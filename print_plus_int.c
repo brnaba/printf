96db1f20ed991b3bb04eecfdb2aeef7e4e6c03e7
Barnaba <39010017+brnaba@users.noreply.github.com>
2023-09-20 00:21:26 +0200

    Update print_plus_int.c

--- print_plus_int.c
+++ print_plus_int.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * prinpint - print integer with plus symbol
- * @arguments: input string
- * @buf: buffer pointer
- * @ibuf: index for buffer pointer
- * Return: number of chars printed
+ * prinpint - print integer with plus symbol.
+ * @arguments: input string.
+ * @buf: buffer pointer.
+ * @ibuf: index for buffer pointer.
+ * Return: number of chars printed.
  */
 int prinpint(va_list arguments, char *buf, unsigned int ibuf)
 {
