92bcfea28024ecf69482e4b38eae044e9aacf1c5
Barnaba <39010017+brnaba@users.noreply.github.com>
2023-09-20 00:18:40 +0200

    Update print_short_upx.c

--- print_short_upx.c
+++ print_short_upx.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * prinhupx - prints a short decimal in hexadecimal
- * @arguments: The character to print
- * @buf: buffer pointer
- * @ibuf: index for buffer pointer
- * Return: number of chars printed
+ * prinhupx - prints a short decimal in hexadecimal.
+ * @arguments: The character to print.
+ * @buf: buffer pointer.
+ * @ibuf: index for buffer pointer.
+ * Return: number of chars printed.
  */
 int prinhupx(va_list arguments, char *buf, unsigned int ibuf)
 {
